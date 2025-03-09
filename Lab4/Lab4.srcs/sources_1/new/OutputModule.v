3abec917b0b6dd8b81ce039478bee06555de8456
Pranit Arya <pranit.arya03@gmail.com>
2025-03-09 14:56:15 -0500

    Started Lab 4

---
+++ Lab4/Lab4.srcs/sources_1/new/OutputModule.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/09/2025 07:52:27 PM
+// Design Name: 
+// Module Name: OutputModule
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module OutputModule(
+
+    );
+endmodule
