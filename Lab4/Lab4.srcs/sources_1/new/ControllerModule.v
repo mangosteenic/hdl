5373c63e388aebb9180e9efc6721b86b4ba36dbf
Pranit Arya <pranit.arya03@gmail.com>
2025-03-09 15:17:54 -0500

    Lab 4 input module

---
+++ Lab4/Lab4.srcs/sources_1/new/ControllerModule.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/09/2025 07:52:27 PM
+// Design Name: 
+// Module Name: ControllerModule
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ControllerModule (
+
+    );
+endmodule
