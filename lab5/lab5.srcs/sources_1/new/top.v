3639ca95669f5273a87aac7386e4150625e6ce4b
Hannah Hong Nguyen <147587888+mangosteenic@users.noreply.github.com>
2025-03-27 18:12:54 -0500

    fixed top module

--- lab5/lab5.srcs/sources_1/new/top.v
+++ lab5/lab5.srcs/sources_1/new/top.v
@@ -25,8 +25,8 @@ module top(clk, btns, swtchs, leds, segs, an);
     input[3:0] btns;
     input[7:0] swtchs;
     output[7:0] leds;
-    output reg [6:0] segs;
-    output reg [3:0] an;
+    output [6:0] segs;
+    output [3:0] an;
     
     //might need to change some of these from wires to regs
     wire cs;
@@ -57,18 +57,22 @@ module top(clk, btns, swtchs, leds, segs, an);
 
     // multiplexing
     reg s;
+    reg [6:0] segs_int;
+    reg [3:0] an_int;
     always @(posedge clk) begin
         case(s)
             0: begin
                 s <= 1;
-                an <= 4'b1110;
-                segs <= in0;
+                segs_int <= in0;
+                an_int <= 4'b1110;
             end
             1: begin
                 s <= 0;
-                an <= 4'b1101;
-                segs <= in1;
+                segs_int <= in1;
+                an_int <= 4'b1101;
             end
         endcase
     end
+    assign segs = segs_int;
+    assign an = an_int;
 endmodule
